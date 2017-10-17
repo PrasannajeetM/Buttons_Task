ef563dae7fde3797f755a5b943a9544aa791ed31
PrasannajeetM <32133910+PrasannajeetM@users.noreply.github.com>
2017-10-17 14:24:52 +0100

    Preliminary Analysis Completed

--- Untitled.R
+++ Untitled.R
@@ -1,28 +1,35 @@
+outlier_sds = 3
+
 setwd("~/Documents/GitHub/Buttons_Task/")
 
 library("lsr")
 library("ggplot2")
 library("pwr")
 
-summary.vesper<- data.frame(PP=c(rep(as.numeric(""),6)),
-                          IN.M=c(rep(as.numeric(""),6)),
-                          IN.SD=c(rep(as.numeric(""),6)),
-                          IN.C.M=c(rep(as.numeric(""),6)),
-                          IN.C.SD=c(rep(as.numeric(""),6)),
-                          IN.I.M=c(rep(as.numeric(""),6)),
-                          IN.I.SD=c(rep(as.numeric(""),6)),
-                          DU.M=c(rep(as.numeric(""),6)),
-                          DU.SD=c(rep(as.numeric(""),6)),
-                          DU.C.M=c(rep(as.numeric(""),6)),
-                          DU.C.SD=c(rep(as.numeric(""),6)),
-                          DU.I.M=c(rep(as.numeric(""),6)),
-                          DU.I.SD=c(rep(as.numeric(""),6)),
-                          DI.M=c(rep(as.numeric(""),6)),
-                          DI.SD=c(rep(as.numeric(""),6)),
-                          DI.C.M=c(rep(as.numeric(""),6)),
-                          DI.C.SD=c(rep(as.numeric(""),6)),
-                          DI.I.M=c(rep(as.numeric(""),6)),
-                          DI.I.SD=c(rep(as.numeric(""),6)))
+
+IN.FILES=  list.files(c("Individual"), pattern="*.csv")
+DU.FILES= list.files(c("Dual1"), pattern="*.csv")
+DI.FILES= list.files(c("Dual2"), pattern="*.csv")
+
+summary.vesper<- data.frame(PP=c(rep(as.numeric(""),length(IN.FILES))),
+                          IN.M=c(rep(as.numeric(""),length(IN.FILES))),
+                          IN.SD=c(rep(as.numeric(""),length(IN.FILES))),
+                          IN.C.M=c(rep(as.numeric(""),length(IN.FILES))),
+                          IN.C.SD=c(rep(as.numeric(""),length(IN.FILES))),
+                          IN.I.M=c(rep(as.numeric(""),length(IN.FILES))),
+                          IN.I.SD=c(rep(as.numeric(""),length(IN.FILES))),
+                          DU.M=c(rep(as.numeric(""),length(IN.FILES))),
+                          DU.SD=c(rep(as.numeric(""),length(IN.FILES))),
+                          DU.C.M=c(rep(as.numeric(""),length(IN.FILES))),
+                          DU.C.SD=c(rep(as.numeric(""),length(IN.FILES))),
+                          DU.I.M=c(rep(as.numeric(""),length(IN.FILES))),
+                          DU.I.SD=c(rep(as.numeric(""),length(IN.FILES))),
+                          DI.M=c(rep(as.numeric(""),length(IN.FILES))),
+                          DI.SD=c(rep(as.numeric(""),length(IN.FILES))),
+                          DI.C.M=c(rep(as.numeric(""),length(IN.FILES))),
+                          DI.C.SD=c(rep(as.numeric(""),length(IN.FILES))),
+                          DI.I.M=c(rep(as.numeric(""),length(IN.FILES))),
+                          DI.I.SD=c(rep(as.numeric(""),length(IN.FILES))))
 
 IN.FILES=  list.files(c("Individual"), pattern="*.csv")
 DU.FILES= list.files(c("Dual1"), pattern="*.csv")
@@ -31,7 +38,8 @@ DI.FILES= list.files(c("Dual2"), pattern="*.csv")
 
 for (i in 1: length(IN.FILES)){
 
- summary.vesper$PP[i]= as.character(IN.FILES[i])
+  pp<-as.character(IN.FILES[i])
+  summary.vesper$PP[i]= gsub(pattern = "\\.csv$", "", pp)
  
  ind.data<- read.csv(paste("Individual/",IN.FILES[i],sep = ""))
  ind.data<- ind.data[(ind.data$Running.SubTrial.=="b1trails1" | ind.data$Running.SubTrial. == "List2") & ind.data$Subject1ACC==1,]
@@ -41,17 +49,38 @@ for (i in 1: length(IN.FILES)){
                         (ind.data$Running.SubTrial.=="b1trails1" &  ind.data$Screen1_Obj_Colour=="red" & ind.data$Screen_1_Obj_Pos== "w") |
                         (ind.data$Running.SubTrial.=="List2" & ind.data$Screen1_Obj_Colour=="blue" & ind.data$Screen_1_Obj_Pos== "w") | 
                         (ind.data$Running.SubTrial.=="List2" &  ind.data$Screen1_Obj_Colour=="red" & ind.data$Screen_1_Obj_Pos== "e"), "C","I")
- 
- summary.vesper$IN.M[i] <- mean(ind.data$Subject1RT)
- summary.vesper$IN.SD[i] <- sd(na.omit(ind.data$Subject1RT))
- summary.vesper$IN.C.M[i]<- mean(na.omit(ind.data$Subject1RT[ind.data$Type=="C"]))
- summary.vesper$IN.C.SD[i]<- sd(na.omit(ind.data$Subject1RT[ind.data$Type=="C"]))
- summary.vesper$IN.I.M[i]<- mean(na.omit(ind.data$Subject1RT[ind.data$Type=="I"]))
- summary.vesper$IN.I.SD[i]<- sd(na.omit(ind.data$Subject1RT[ind.data$Type=="I"]))
+ m<-mean(ind.data$Subject1RT)
+ s<-sd(na.omit(ind.data$Subject1RT))
+ summary.vesper$IN.M[i] <- mean(ind.data$Subject1RT[ind.data$Subject1RT> m-outlier_sds*s &
+                                                    ind.data$Subject1RT< m+outlier_sds*s])
+ summary.vesper$IN.SD[i] <- sd(na.omit(ind.data$Subject1RT[ind.data$Subject1RT> m-outlier_sds*s &
+                                                           ind.data$Subject1RT< m+outlier_sds*s]))
+ summary.vesper$IN.C.M[i]<- mean(na.omit(ind.data$Subject1RT[ind.data$Type=="C" & 
+                                                             ind.data$Subject1RT> m-outlier_sds*s &
+                                                             ind.data$Subject1RT< m+outlier_sds*s]))
+ summary.vesper$IN.C.SD[i]<- sd(na.omit(ind.data$Subject1RT[ind.data$Type=="C" &
+                                                            ind.data$Subject1RT> m-outlier_sds*s &
+                                                            ind.data$Subject1RT< m+outlier_sds*s]))
+ summary.vesper$IN.I.M[i]<- mean(na.omit(ind.data$Subject1RT[ind.data$Type=="I" &
+                                                             ind.data$Subject1RT> m-outlier_sds*s &
+                                                             ind.data$Subject1RT< m+outlier_sds*s]))
+ summary.vesper$IN.I.SD[i]<- sd(na.omit(ind.data$Subject1RT[ind.data$Type=="I" &
+                                                            ind.data$Subject1RT> m-outlier_sds*s &
+                                                            ind.data$Subject1RT< m+outlier_sds*s]))
  
 }
 
-dualu.data<- read.csv(paste("Dual1/",DU.FILES[1],sep = ""))
+
+for (i in 1:length(DU.FILES)){
+
+dualu.data<- read.csv(paste("Dual1/",DU.FILES[i],sep = ""))
+
+name<- gsub("[^[:digit:]]", "", DU.FILES[i])
+
+
+x<-as.numeric(match(substr(name,1,3), summary.vesper$PP))
+y<-as.numeric(match(substr(name,4,6), summary.vesper$PP))
+
 dualu.data<- dualu.data[(dualu.data$Running.SubTrial.=="CM1Trails" | dualu.data$Running.SubTrial. == "CM2Trials") & dualu.data$Subject1ACC==1 & dualu.data$Subject2ACC==1,]
 dualu.data$Subject1RT<- as.numeric(dualu.data$Subject1RT)
 dualu.data$Subject2RT<- as.numeric(dualu.data$Subject2RT)
@@ -66,18 +95,108 @@ dualu.data$Type2<- ifelse((dualu.data$Running.SubTrial.=="CM1Trails" & dualu.dat
                             (dualu.data$Running.SubTrial.=="CM2Trials" & dualu.data$Screen2_Obj_Colour=="blue" & dualu.data$Screen_2_Obj_Pos== "w") | 
                             (dualu.data$Running.SubTrial.=="CM2Trials" &  dualu.data$Screen2_Obj_Colour=="red" & dualu.data$Screen_2_Obj_Pos== "e"), "C","I")
 
-
+m<-mean(dualu.data$Subject1RT)
+s<-sd(na.omit(dualu.data$Subject1RT))
+m1<-mean(dualu.data$Subject2RT)
+s1<-sd(na.omit(dualu.data$Subject2RT))
    
-   summary.vesper$DU.M[1] <- mean(dualu.data$Subject1RT)
-   summary.vesper$DU.SD[1] <- sd(na.omit(dualu.data$Subject1RT))
-   summary.vesper$DU.M[2] <- mean(dualu.data$Subject2RT)
-   summary.vesper$DU.SD[2] <- sd(na.omit(dualu.data$Subject2RT))
-   summary.vesper$DU.C.M[1]<- mean(na.omit(dualu.data$Subject1RT[dualu.data$Type1=="C"]))
-   summary.vesper$DU.C.SD[1]<- sd(na.omit(dualu.data$Subject1RT[dualu.data$Type1=="C"]))
-   summary.vesper$DU.C.M[2]<- mean(na.omit(dualu.data$Subject2RT[dualu.data$Type2=="C"]))
-   summary.vesper$DU.C.SD[2]<- sd(na.omit(dualu.data$Subject2RT[dualu.data$Type2=="C"]))
-   summary.vesper$DU.I.M[1]<- mean(na.omit(dualu.data$Subject1RT[dualu.data$Type1=="I"]))
-   summary.vesper$DU.I.SD[1]<- sd(na.omit(dualu.data$Subject1RT[dualu.data$Type1=="I"]))
-   summary.vesper$DU.I.M[2]<- mean(na.omit(dualu.data$Subject2RT[dualu.data$Type2=="I"]))
-   summary.vesper$DU.I.SD[2]<- sd(na.omit(dualu.data$Subject2RT[dualu.data$Type2=="I"]))
+   summary.vesper$DU.M[x] <- mean(dualu.data$Subject1RT[dualu.data$Subject1RT> m-outlier_sds*s &
+                                                        dualu.data$Subject1RT< m+outlier_sds*s])
+   summary.vesper$DU.SD[x] <- sd(na.omit(dualu.data$Subject1RT[dualu.data$Subject1RT> m-outlier_sds*s &
+                                                               dualu.data$Subject1RT< m+outlier_sds*s]))
+   summary.vesper$DU.M[y] <- mean(dualu.data$Subject2RT[dualu.data$Subject2RT> m1-outlier_sds*s1 &
+                                                        dualu.data$Subject2RT< m1+outlier_sds*s1])
+   summary.vesper$DU.SD[y] <- sd(na.omit(dualu.data$Subject2RT[dualu.data$Subject2RT> m1-outlier_sds*s1 &
+                                                               dualu.data$Subject2RT< m1+outlier_sds*s1]))
+   summary.vesper$DU.C.M[x]<- mean(na.omit(dualu.data$Subject1RT[dualu.data$Type1=="C" &
+                                                                   dualu.data$Subject1RT> m-outlier_sds*s &
+                                                                   dualu.data$Subject1RT< m+outlier_sds*s]))
+   summary.vesper$DU.C.SD[x]<- sd(na.omit(dualu.data$Subject1RT[dualu.data$Type1=="C" &
+                                                                  dualu.data$Subject1RT> m-outlier_sds*s &
+                                                                  dualu.data$Subject1RT< m+outlier_sds*s]))
+   summary.vesper$DU.C.M[y]<- mean(na.omit(dualu.data$Subject2RT[dualu.data$Type2=="C" &
+                                                                 dualu.data$Subject2RT> m1-outlier_sds*s1 &
+                                                                 dualu.data$Subject2RT< m1+outlier_sds*s1]))
+   summary.vesper$DU.C.SD[y]<- sd(na.omit(dualu.data$Subject2RT[dualu.data$Type2=="C" &
+                                                                dualu.data$Subject2RT> m1-outlier_sds*s1 &
+                                                                dualu.data$Subject2RT< m1+outlier_sds*s1]))
+   summary.vesper$DU.I.M[x]<- mean(na.omit(dualu.data$Subject1RT[dualu.data$Type1=="I" &
+                                                                   dualu.data$Subject1RT> m-outlier_sds*s &
+                                                                   dualu.data$Subject1RT< m+outlier_sds*s]))
+   summary.vesper$DU.I.SD[x]<- sd(na.omit(dualu.data$Subject1RT[dualu.data$Type1=="I" &
+                                                                  dualu.data$Subject1RT> m-outlier_sds*s &
+                                                                  dualu.data$Subject1RT< m+outlier_sds*s]))
+   summary.vesper$DU.I.M[y]<- mean(na.omit(dualu.data$Subject2RT[dualu.data$Type2=="I" &
+                                                                 dualu.data$Subject2RT> m1-outlier_sds*s1 &
+                                                                 dualu.data$Subject2RT< m1+outlier_sds*s1]))
+   summary.vesper$DU.I.SD[y]<- sd(na.omit(dualu.data$Subject2RT[dualu.data$Type2=="I" &
+                                                                dualu.data$Subject2RT> m1-outlier_sds*s1 &
+                                                                dualu.data$Subject2RT< m1+outlier_sds*s1]))
  
+}
+
+
+for (i in 1:length(DI.FILES)){
+  
+  duali.data<- read.csv(paste("Dual2/",DI.FILES[i],sep = ""))
+  
+  name<- gsub("[^[:digit:]]", "", DI.FILES[i])
+  
+  
+  x<-as.numeric(match(substr(name,1,3), summary.vesper$PP))
+  y<-as.numeric(match(substr(name,4,6), summary.vesper$PP))
+  
+  duali.data<- duali.data[(duali.data$Running.SubTrial.=="CM1Trails" | duali.data$Running.SubTrial. == "CM2Trials") & duali.data$Subject1ACC==1 & duali.data$Subject2ACC==1,]
+  duali.data$Subject1RT<- as.numeric(duali.data$Subject1RT)
+  duali.data$Subject2RT<- as.numeric(duali.data$Subject2RT)
+  
+  duali.data$Type1<- ifelse((duali.data$Running.SubTrial.=="CM1Trails" & duali.data$Screen1_Obj_Colour=="blue" & duali.data$Screen_1_Obj_Pos== "e") | 
+                              (duali.data$Running.SubTrial.=="CM1Trails" &  duali.data$Screen1_Obj_Colour=="red" & duali.data$Screen_1_Obj_Pos== "w") |
+                              (duali.data$Running.SubTrial.=="CM2Trials" & duali.data$Screen1_Obj_Colour=="blue" & duali.data$Screen_1_Obj_Pos== "w") | 
+                              (duali.data$Running.SubTrial.=="CM2Trials" &  duali.data$Screen1_Obj_Colour=="red" & duali.data$Screen_1_Obj_Pos== "e"), "C","I")
+  
+  duali.data$Type2<- ifelse((duali.data$Running.SubTrial.=="CM1Trails" & duali.data$Screen2_Obj_Colour=="blue" & duali.data$Screen_2_Obj_Pos== "e") | 
+                              (duali.data$Running.SubTrial.=="CM1Trails" &  duali.data$Screen2_Obj_Colour=="red" & duali.data$Screen_2_Obj_Pos== "w") |
+                              (duali.data$Running.SubTrial.=="CM2Trials" & duali.data$Screen2_Obj_Colour=="blue" & duali.data$Screen_2_Obj_Pos== "w") | 
+                              (duali.data$Running.SubTrial.=="CM2Trials" &  duali.data$Screen2_Obj_Colour=="red" & duali.data$Screen_2_Obj_Pos== "e"), "C","I")
+  
+  m<-mean(duali.data$Subject1RT)
+  s<-sd(na.omit(duali.data$Subject1RT))
+  m1<-mean(duali.data$Subject2RT)
+  s1<-sd(na.omit(duali.data$Subject2RT))
+  
+  
+  summary.vesper$DI.M[x] <- mean(duali.data$Subject1RT[duali.data$Subject1RT> m-outlier_sds*s &
+                                                         duali.data$Subject1RT< m+outlier_sds*s])
+  summary.vesper$DI.SD[x] <- sd(na.omit(duali.data$Subject1RT[duali.data$Subject1RT> m-outlier_sds*s &
+                                                                duali.data$Subject1RT< m+outlier_sds*s]))
+  summary.vesper$DI.M[y] <- mean(duali.data$Subject2RT[duali.data$Subject2RT> m1-outlier_sds*s1 &
+                                                         duali.data$Subject2RT< m1+outlier_sds*s1])
+  summary.vesper$DI.SD[y] <- sd(na.omit(duali.data$Subject2RT[duali.data$Subject2RT> m1-outlier_sds*s1 &
+                                                                duali.data$Subject2RT< m1+outlier_sds*s1]))
+  summary.vesper$DI.C.M[x]<- mean(na.omit(duali.data$Subject1RT[duali.data$Type1=="C" &
+                                                                  duali.data$Subject1RT> m-outlier_sds*s &
+                                                                  duali.data$Subject1RT< m+outlier_sds*s]))
+  summary.vesper$DI.C.SD[x]<- sd(na.omit(duali.data$Subject1RT[duali.data$Type1=="C" &
+                                                                 duali.data$Subject1RT> m-outlier_sds*s &
+                                                                 duali.data$Subject1RT< m+outlier_sds*s]))
+  summary.vesper$DI.C.M[y]<- mean(na.omit(duali.data$Subject2RT[duali.data$Type2=="C" &
+                                                                  duali.data$Subject2RT> m1-outlier_sds*s1 &
+                                                                  duali.data$Subject2RT< m1+outlier_sds*s1]))
+  summary.vesper$DI.C.SD[y]<- sd(na.omit(duali.data$Subject2RT[duali.data$Type2=="C" &
+                                                                 duali.data$Subject2RT> m1-outlier_sds*s1 &
+                                                                 duali.data$Subject2RT< m1+outlier_sds*s1]))
+  summary.vesper$DI.I.M[x]<- mean(na.omit(duali.data$Subject1RT[duali.data$Type1=="I" &
+                                                                  duali.data$Subject1RT> m-outlier_sds*s &
+                                                                  duali.data$Subject1RT< m+outlier_sds*s]))
+  summary.vesper$DI.I.SD[x]<- sd(na.omit(duali.data$Subject1RT[duali.data$Type1=="I" &
+                                                                 duali.data$Subject1RT> m-outlier_sds*s &
+                                                                 duali.data$Subject1RT< m+outlier_sds*s]))
+  summary.vesper$DI.I.M[y]<- mean(na.omit(duali.data$Subject2RT[duali.data$Type2=="I" &
+                                                                  duali.data$Subject2RT> m1-outlier_sds*s1 &
+                                                                  duali.data$Subject2RT< m1+outlier_sds*s1]))
+  summary.vesper$DI.I.SD[y]<- sd(na.omit(duali.data$Subject2RT[duali.data$Type2=="I" &
+                                                                 duali.data$Subject2RT> m1-outlier_sds*s1 &
+                                                                 duali.data$Subject2RT< m1+outlier_sds*s1]))
+  
+}
